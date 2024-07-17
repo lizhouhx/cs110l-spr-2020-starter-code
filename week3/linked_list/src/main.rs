9a937fe2e766a14700ae7f5b6f181040af8d18fe
lizhouhx <lizhouhx@126.com>
2024-07-17 17:33:19 +0800

    week3_part2

--- week3/linked_list/src/main.rs
+++ week3/linked_list/src/main.rs
@@ -1,12 +1,12 @@
-use linked_list::LinkedList;
+use linked_list::{ComputeNorm, LinkedList};
 pub mod linked_list;
 
 fn main() {
-    let mut list: LinkedList<u32> = LinkedList::new();
+    let mut list: LinkedList<String> = LinkedList::new();
     assert!(list.is_empty());
     assert_eq!(list.get_size(), 0);
     for i in 1..12 {
-        list.push_front(i);
+        list.push_front(i.to_string());
     }
     println!("{}", list);
     println!("list size: {}", list.get_size());
@@ -14,9 +14,17 @@ fn main() {
     println!("{}", list);
     println!("size: {}", list.get_size());
     println!("{}", list.to_string()); // ToString impl for anything impl Display
+    println!("list_clone is {}",list.clone());
 
-    // If you implement iterator trait:
-    //for val in &list {
-    //    println!("{}", val);
-    //}
+    //If you implement iterator trait:
+    for val in &list {
+       println!("iterator test: {}", val);
+    }
+
+    //Test compute_norm
+    let mut f64_list:LinkedList<f64> = LinkedList::new();
+    f64_list.push_front(3.0);
+    f64_list.push_front(4.0);
+    f64_list.push_front(5.0);
+    println!("f64_list's compute_norm result is {}", f64_list.compute_norm());
 }
